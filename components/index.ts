b7611a4c352f8386f9302967a7e7b2687cd57a41
Vadim <vadim.slisarenko.03@gmail.com>
2023-11-23 11:38:23 +0200

    last commit car_showchase

---
+++ components/index.ts
@@ -0,0 +1,24 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
+import SearchManufacturer from "@/components/SearchManufacturer";
+import CarCard from "@/components/CarCard";
+import CarDetails from "@/components/CarDetails";
+import ShowMore from "@/components/ShowMore";
+
+
+export {
+    ShowMore,
+    CarDetails,
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+}
